67a6d07b90b1e0b84c0d72c428361b75b4c638e9
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-07 12:18:15 -0500

    changed ports to push to heroku

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
 const server = require('./server.js');
 
-server.listen(5000, () => {
-	console.log('Server is up on port 5000');
+// server.listen(5000, () => {
+// 	console.log('Server is up on port 5000');
+// });
+const port = process.env.PORT || 5002;
+server.listen(port, () => {
+	console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,34 +1,86 @@
 const express = require('express');
-const User = require('./userDb');
-
+const Users = require('./userDb');
+const Posts = require('../posts/postDb');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
 	// do your magic!
+	Users.insert(req.body)
+		.then((user) => {
+			res.status(201).json(user);
+		})
+		.catch((err) => {
+			res.status(400).json({ error: 'There was a problem adding user' });
+		});
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
 	// do your magic!
+	req.body.user_id = req.user.id;
+	Posts.insert(req.body)
+		.then((posted) => {
+			res.status(201).json(posted);
+		})
+		.catch((err) => {
+			res
+				.status(500)
+				.json({ message: ' an error occured while making new post' });
+		});
 });
 
 router.get('/', (req, res) => {
 	// do your magic!
+	Users.get()
+		.then((users) => {
+			res.status(200).json(users);
+		})
+		.catch((error) => {
+			res.status(500).json({ error: 'There was a problem gtting the users' });
+		});
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
 	// do your magic!
+	res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUser, (req, res) => {
 	// do your magic!
+	const user_id = req.params.id;
+	Users.getUserPosts(user_id)
+		.then((post) => {
+			res.status(200).json(post);
+		})
+		.catch((err) => {
+			res
+				.status(500)
+				.json({ errorMessage: ' There was an error getting the post' });
+		});
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
 	// do your magic!
+	Users.remove(req.params.id)
+		.then((user) => {
+			res.status(200).json(user);
+		})
+		.catch((err) => {
+			res
+				.status(400)
+				.json({ errorMessage: 'There was a problem deleting user' });
+		});
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
 	// do your magic!
+	const { id } = req.params;
+	Users.update(id, req.body)
+		.then((user) => {
+			res.status(200).json(user);
+		})
+		.catch((error) => {
+			res.status(400).json({ MESSAGE: 'There was a problem updating user' });
+		});
 });
 
 //custom middleware
@@ -36,7 +88,7 @@ router.put('/:id', (req, res) => {
 function validateUserId(req, res, next) {
 	// do your magic!
 	const { id } = req.params;
-	User.getById(id).then((user) => {
+	Users.getById(id).then((user) => {
 		if (user) {
 			req.user = user;
 			next();
