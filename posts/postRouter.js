becc955a8bf13e66b01548a02e86aad5a4f6e898
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-07 13:59:53 -0500

    MVP + Posts stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,69 @@
 const express = require('express');
-
+const Posts = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+	// do your magic!
+	Posts.get()
+		.then((users) => {
+			res.status(200).json(users);
+		})
+		.catch((error) => {
+			res.status(404).json({ errorMessage: 'Users not found' });
+		});
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+	// do your magic!
+	res.status(200).json(req.post);
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+	// do your magic!
+	Posts.remove(req.params.id).then((post) => {
+		res.status(200).json(post);
+	});
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, validatePost, (req, res) => {
+	// do your magic!
+	const { id } = req.params;
+	Posts.update(id, req.body)
+		.then((user) => {
+			res.status(200).json(user);
+		})
+		.catch((error) => {
+			res
+				.status(400)
+				.json({ errorMessage: 'There was a problem updating user' });
+		});
 });
 
 // custom middleware
-
-function validatePostId(req, res, next) {
-  // do your magic!
+function validatePost(req, res, next) {
+	// do your magic!
+	if (!Object.keys(req.body).length) {
+		res.status(400).json({ message: 'missing post data' });
+	} else if (!req.body.text) {
+		req.status(400).json({ message: 'missing required text' });
+	} else {
+		next();
+	}
 }
-
+function validatePostId(req, res, next) {
+	// do your magic!
+	const { id } = req.params;
+	Posts.getById(id)
+		.then((post) => {
+			if (post) {
+				req.post = post;
+				next();
+			} else {
+				res.status(400).json({ errorMessage: 'invalid id' });
+			}
+		})
+		.catch((error) => {
+			res.status(400).json({ errorMessage: 'There was an error with the id' });
+		});
+}
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -113,7 +113,7 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
 	// do your magic!
-	if (Object.keys(req.body).length === 0) {
+	if (!Object.keys(req.body).length === 0) {
 		res.status(400).json({ message: 'missing post data' });
 	} else if (!req.body.text) {
 		req.status(400).json({ message: 'missing required text' });
