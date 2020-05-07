ab759b6419376ec2b6263c01585898d288c6f11a
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-06 22:36:52 -0500

    Initial commit - set up server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code away!
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.json(`Express middleware`);
+});
+
+server.listen(5000, () => {
+	console.log('Server is up on port 5000');
+});
